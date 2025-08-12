05120a3985a3a29668674024146ae0e05b98731f
Uri <uri@uri.com>
2025-08-12 09:19:26 +0200

    Day2 done

--- 2015/02/day2_1.js
+++ 2015/02/day2_1.js
@@ -1,15 +1,27 @@
-import { readTestFile,readProdFile } from "./day2.js";
+import { readTestFile, readProdFile } from "./day2.js";
 
-let data = await readTestFile();
-//let data = await readProdFile();
+//let data = await readTestFile();
+let data = await readProdFile();
 
-let bob = data.split("");
+let bob = data.split("\n");
 let counter = 0;
 
-console.log(data);
+for (let index = 0; index < bob.length; index++) {
+  const element = bob[index];
+  let lwh = element.split("x");
+  let a = parseInt(lwh[0]);
+  let b = parseInt(lwh[1]);
+  let c = parseInt(lwh[2]);
+  let numlwh = [a, b, c];
 
-//console.log("result: "+counter);
+  numlwh = numlwh.sort(function(a, b){return a - b});
 
-if(counter===1586300){
-console.log("*Solved*");
+  counter += (2 * a * b) + (2 * b * c) + (2 * c * a);
+  counter += numlwh[0] * numlwh[1];
+}
+
+console.log("result: " + counter);
+
+if (counter === 1586300) {
+  console.log("*Solved*");
 }
--- 2015/02/day2_2.js
+++ 2015/02/day2_2.js
@@ -1,15 +1,40 @@
-import { readTestFile,readProdFile } from "./day2.js";
+import { readTestFile, readProdFile } from "./day2.js";
 
-//let data = await readTestFile();
-let data = await readProdFile();
+let prod = true;
+let data;
 
-var bob = data.split("");
-var counter = 0;
+if(prod == true){
+    data = await readProdFile();
+}else{
+    data = await readTestFile();
+}
 
 
+let bob = data.split("\n");
+let counter = 0;
 
-console.log("result: "+counter);
+for (let index = 0; index < bob.length; index++) {
+  const element = bob[index];
+  let lwh = element.split("x");
+  let a = parseInt(lwh[0]);
+  let b = parseInt(lwh[1]);
+  let c = parseInt(lwh[2]);
+  let numlwh = [a, b, c];
 
-if(counter===3737498){
-console.log("*Solved*");
+  numlwh = numlwh.sort(function (a, b) {
+    return a - b;
+  });
+
+  counter += a * b * c;
+  counter += numlwh[0] + numlwh[1] + numlwh[0] + numlwh[1];
+}
+
+console.log("result: " + counter);
+
+if (counter === 3737498) {
+  console.log("*Solved*");
+} else if (counter === 48) {
+  console.log("*Test OK*");
+} else {
+  console.log("*Nope*");
 }
